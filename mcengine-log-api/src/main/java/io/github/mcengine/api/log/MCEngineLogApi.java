6f297768305a16eee7a268068f05023334e60a25
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-10 11:46:44 +0000

    Added func MCEngineLogApi

--- mcengine-log-api/src/main/java/io/github/mcengine/api/log/MCEngineLogApi.java
+++ mcengine-log-api/src/main/java/io/github/mcengine/api/log/MCEngineLogApi.java
@@ -1,6 +1,14 @@
 package io.github.mcengine.api.log;
 
+import org.bukkit.plugin.Plugin;
+
 /**
  * Utility class for various API-related operations in the MCEngineLog plugin.
  */
-public class MCEngineLogApi {}
+public class MCEngineLogApi {
+    private Plugin plugin;
+
+    public MCEngineLogApi(Plugin plugin) {
+        this.plugin = plugin;
+    }
+}
