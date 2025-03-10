d0e5447243ef8864e595c08d8eeb2b256b2bcf2c
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-10 12:03:48 +0000

    Fix code logic

--- mcengine-log-api/src/main/java/io/github/mcengine/api/log/database/sqlite/MCEngineLogSQLite.java
+++ mcengine-log-api/src/main/java/io/github/mcengine/api/log/database/sqlite/MCEngineLogSQLite.java
@@ -1,14 +1,25 @@
 package io.github.mcengine.api.log.database.sqlite;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
 /* 
  * The {@code MCEngineLogSQLite} is designed for SQLite functions to manage and store logs in logs.sqlite.
  */
 public class MCEngineLogSQLite {
 
     private Plugin plugin;
-    private String dbPath = config.getString("dbPath", "logs.sqlite");
+    private String dbPath;
 
     public MCEngineLogSQLite(Plugin plugin) {
         this.plugin = plugin;
+        FileConfiguration config = plugin.getConfig();
+        this.dbPath = config.getString("dbPath", "logs.sqlite");
     }
 }
