51d3a60c22cb5b30c6824de47656864e8b810b89
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-10 11:45:10 +0000

    Added comment

--- mcengine-log-api/src/main/java/io/github/mcengine/api/log/database/sqlite/MCEngineLogSQLite.java
+++ mcengine-log-api/src/main/java/io/github/mcengine/api/log/database/sqlite/MCEngineLogSQLite.java
@@ -1,3 +1,6 @@
 package io.github.mcengine.api.log.database.sqlite;
 
+/* 
+ * The {@code MCEngineLogSQLite} is designed for SQLite functions to manage and store logs in logs.sqlite.
+ */
 public class MCEngineLogSQLite {}
